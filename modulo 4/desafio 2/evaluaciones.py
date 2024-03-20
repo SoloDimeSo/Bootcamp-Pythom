98f463ddb3305320b870d9f5d74f2fa25edbce7f
SoloDimeSo <sparedesgonzalez@gmail.com>
2024-03-20 18:27:31 -0300

    temine

--- modulo 4/desafio 2/evaluaciones.py
+++ modulo 4/desafio 2/evaluaciones.py
@@ -2,13 +2,13 @@ from pizza import Pizza
 
 
 print("Atributos de clase de la clase Pizza:")
-print("Ingredientes proteicos posibles:", Pizza.ingredientes_proteicos_posibles)
-print("Ingredientes vegetales posibles:", Pizza.ingredientes_vegetales_posibles)
+print("Carnes posibles:", Pizza.ingredientes_proteicos_posibles)
+print("Vegetales posibles:", Pizza.ingredientes_vegetales_posibles)
 print("Tipos de masa posibles:", Pizza.tipos_de_masa_posibles)
 
 
-print("\nValidación de elemento 'salsa de tomate' en ['salsa de tomate', 'salsa bbq']:",
-    Pizza.validar_elemento("salsa de tomate", ["salsa de tomate", "salsa bbq"]))
+print("\n Validacion de Salsas:  'salsa de tomate' en ['salsa de tomate', 'salsa barbecue']:",
+    Pizza.pizza_valida("salsa de tomate", ["salsa de tomate", "salsa barbecue"]))
 
 
 pizza = Pizza()
@@ -16,7 +16,7 @@ pizza.hacer_pedido()
 
 
 print("\nIngredientes de la pizza:")
-print("Ingrediente proteico:", pizza.ingrediente_proteico)
-print("Ingredientes vegetales:", pizza.ingrediente_vegetal_1, "y", pizza.ingrediente_vegetal_2)
-print("Tipo de masa:", pizza.tipo_de_masa)
+print("Carnes: ", pizza.ingrediente_proteico)
+print("Vegetales: ", pizza.ingrediente_vegetal_1, "y", pizza.ingrediente_vegetal_2)
+print("Masa: ", pizza.tipo_de_masa)
 print("Es una pizza válida:", pizza.es_valida)
--- modulo 4/desafio 2/pizza.py
+++ modulo 4/desafio 2/pizza.py
@@ -11,23 +11,23 @@ class Pizza:
         self.es_valida = None
 
     @staticmethod
-    def validar_elemento(elemento, valores_posibles):
+    def pizza_valida(elemento, valores_posibles):
         return elemento in valores_posibles
 
     def hacer_pedido(self):
-        self.ingrediente_proteico = input("Ingrese el ingrediente proteico (pollo, vacuno, carne vegetal): ")
-        self.ingrediente_vegetal_1 = input("Ingrese el primer ingrediente vegetal (tomate, aceitunas, champiñones): ")
-        self.ingrediente_vegetal_2 = input("Ingrese el segundo ingrediente vegetal (tomate, aceitunas, champiñones): ")
+        self.ingrediente_proteico = input("Ingrese tipo de Carne (pollo, vacuno, carne vegetal): ")
+        self.ingrediente_vegetal_1 = input("Ingrese tipo de vegetal (tomate, aceitunas, champiñones): ")
+        self.ingrediente_vegetal_2 = input("Ingrese el segundo Vegetal (tomate, aceitunas, champiñones): ")
         self.tipo_de_masa = input("Ingrese el tipo de masa (tradicional, delgada): ")
 
         self.es_valida = self.validar_pedido()
 
     def validar_pedido(self):
         ingredientes_validos = (
-            self.validar_elemento(self.ingrediente_proteico, self.ingredientes_proteicos_posibles) and
-            self.validar_elemento(self.ingrediente_vegetal_1, self.ingredientes_vegetales_posibles) and
-            self.validar_elemento(self.ingrediente_vegetal_2, self.ingredientes_vegetales_posibles) and
-            self.validar_elemento(self.tipo_de_masa, self.tipos_de_masa_posibles)
+            self.pizza_valida(self.ingrediente_proteico, self.ingredientes_proteicos_posibles) and
+            self.pizza_valida(self.ingrediente_vegetal_1, self.ingredientes_vegetales_posibles) and
+            self.pizza_valida(self.ingrediente_vegetal_2, self.ingredientes_vegetales_posibles) and
+            self.pizza_valida(self.tipo_de_masa, self.tipos_de_masa_posibles)
         )
         return ingredientes_validos
 
